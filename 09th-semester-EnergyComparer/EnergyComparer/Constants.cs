339bda641ecdf2724bcd81379cda016073d29d27
KuskIV <madshkusk@gmail.com>
2022-09-29 15:13:06 +0200

    wifi off

---
+++ 09th-semester-EnergyComparer/EnergyComparer/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyComparer
+{
+    public static class Constants
+    {
+        public static TimeSpan TimeBetweenExperiments = TimeSpan.FromMinutes(30);
+        public static TimeSpan DurationOfExperiments = TimeSpan.FromMinutes(15);
+    }
+}
