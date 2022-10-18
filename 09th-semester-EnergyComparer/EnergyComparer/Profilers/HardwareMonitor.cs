2f1d61a38462e1c98c5279558240be47348bdea5
KuskIV <madshkusk@gmail.com>
2022-10-18 09:51:54 +0200

    hardwaremonitorservice

---
+++ 09th-semester-EnergyComparer/EnergyComparer/Profilers/HardwareMonitor.cs
@@ -0,0 +1,43 @@
+﻿using EnergyComparer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyComparer.Profilers
+{
+    public class HardwareMonitor : IEnergyProfiler
+    {
+        private System.Timers.Timer _timer;
+        private readonly int IntervalBetweenReadsInSeconds = 1;
+
+        public string GetName()
+        {
+            return EWindowsProfilers.HardwareMonitor.ToString();
+        }
+
+        public DtoRawData ParseCsv(string path, int experimentId, DateTime startTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(DateTime date)
+        {
+            _timer = new System.Timers.Timer(1000 * IntervalBetweenReadsInSeconds); // 10 seconds
+            _timer.Elapsed += timer_Elapsed;
+            _timer.Enabled = true;
+            Console.WriteLine("Timer has started");
+        }
+
+        public void Stop()
+        {
+            _timer.Enabled = false;
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.Write("doing gods work");
+        }
+    }
+}
--- 09th-semester-EnergyComparer/TestClient/Program.cs
+++ 09th-semester-EnergyComparer/TestClient/Program.cs
@@ -4,6 +4,8 @@
 using CsvHelper;
 using EnergyComparer.Models;
 using EnergyComparer.Profilers;
+using EnergyComparer.Services;
+using Serilog.Core;
 using System.Formats.Asn1;
 using System.Globalization;
 using System.IO;
@@ -13,7 +15,20 @@ using System.IO;
 
 //var data = intel.ParseCsv("C:\\Users\\Mads Kusk\\Documents\\09-experiment-data\\IntelPowerGadget\\2022-09-30-12-20-15.csv", 1, DateTime.UtcNow);
 
+var hwm = new HardwareMonitor();
+var service = new HardwareMonitorService(null);
 
+service.GetCoreTemperatures();
 
+hwm.Start(DateTime.UtcNow);
+
+await Task.Delay(TimeSpan.FromSeconds(20));
+Console.WriteLine("retrying");
+hwm.Stop();
+
+while (true)
+{
+    await Task.Delay(1000);
+}
 
 Console.WriteLine("Hello, World!");
