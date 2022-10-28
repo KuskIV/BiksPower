75c6cb883e9afeedeeb211c0b52e1bf3576ed3f3
Jeppe <jeppe@bandsholm.com>
2022-10-28 16:05:07 +0200

    E3 and stuff

--- 09th-semester-EnergyComparer/EnergyComparer/Profilers/E3.cs
+++ 09th-semester-EnergyComparer/EnergyComparer/Profilers/E3.cs
@@ -14,6 +14,9 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using ILogger = Serilog.ILogger;
 
 namespace EnergyComparer.Profilers
 {
@@ -25,6 +28,9 @@ namespace EnergyComparer.Profilers
         private string fileType;
         private string fileStart;
         private string fileEnd;
+        private List<E3Data> result;
+        private DateTime PrevMeasure;
+
         public E3() 
         {
             _source = EWindowsProfilers.E3;
@@ -47,34 +53,61 @@ namespace EnergyComparer.Profilers
         public async Task WaitForStart(DateTime date) 
         {
             dateTime = date;
-            await WaitForBlock();
+            //var clear = E3ClearProccesOld();
+            //clear.Start();
+            //string error = "";
+            //Console.WriteLine($"Clearing of old data starting attempts at {DateTime.UtcNow}");
+            //do
+            //{
+            //    error = clear.StandardError.ReadToEnd();
+            //    if (error.Length > 0) 
+            //    {
+            //        Console.WriteLine($"Error orrcurred at clearing trying again Time: {DateTime.UtcNow}");
+            //    }
+            //}
+            //while (error.Length > 0);
+            //Console.WriteLine($"Clearing completed at: {DateTime.UtcNow}");
+            //await clear.WaitForExitAsync();
+            PrevMeasure = await WaitForBlock();
             Start(date);
         }
         public async Task WaitForStop() 
         {
-            await WaitForBlock();
+            _ = await WaitForBlock();
             Stop();
         }
 
-        private async Task WaitForBlock() 
+        private async Task<DateTime> WaitForBlock() 
         {
             string path = Constants.GetPathForSource(_source.ToString())+"\\temp";
             var start = E3Save(path, "1");
             var exit = E3Save(path, "2");
 
             start.Start();
-            while (!NewBlock(path,"1","2"))
+            await start.WaitForExitAsync();
+            do
             {
                 await Task.Delay(1);
                 exit.Start();
+                await exit.WaitForExitAsync();
             }
+            while (!NewBlock(path, "1", "2"));
+            Console.WriteLine("New-Block encountered");
+            return GetMostRecentTimstamp(GetE3Data(path,"1"));
         }
 
         private bool NewBlock(string path, string file1, string file2)
-        { 
-            var Initial = GetE3Data(path, file1);
-            var Final = GetE3Data(path, file2);
-            return (Final.GetRange(Initial.Count, Final.Count - Initial.Count).Count < 0);  
+        {
+            try
+            {
+                var Initial = GetMostRecentTimstamp(GetE3Data(path, file1));
+                var Final = GetMostRecentTimstamp(GetE3Data(path, file2));
+                return (Initial.CompareTo(Final) != 0);  
+            }
+            catch (Exception)
+            { 
+                return false;
+            }
         }
 
         public void Stop()
@@ -82,104 +115,35 @@ namespace EnergyComparer.Profilers
             CollectLogs();
         }
 
-        private void Clear() 
+        private DateTime GetMostRecentTimstamp(List<E3Data> e3Datas) 
         {
-            string path = Constants.GetPathForSource(_source.ToString());
-            Process save = E3Save(path, "tempOld");
-            save.Start();
-            save.WaitForExit();
-            //Process clear = E3ClearProcces();
-            //if (File.Exists(SruDefaultPath+"\\SRUDB.dat"))
-            //{
-            //    clear.Start();
-            //    clear.WaitForExit();
-            //}
+            return GetTimeStamp(e3Datas.Last());
         }
 
-        //private Process E3ClearProcces()
-        //{
-            
-        //}
+        private DateTime GetTimeStamp(E3Data e3Data) 
+        {
+            string format = "yyyy-MM-dd:HH:mm:ss.ffff";//2022-10-28:11:58:00.0000
 
-        //private void StartLogging() 
-        //{
-        //    Process start = E3StartProcess();
-        //    start.Start();
-        //    start.WaitForExit();
-        //}
+            if (DateTime.TryParseExact(e3Data.TimeStamp, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
+            {
+                return dateTime;
+            }
+            else
+            {
+                return DateTime.MinValue;
+            }
+        }
 
         private void CollectLogs() 
         {
-            string path = Constants.GetPathForSource(_source.ToString());
-            Process save = E3Save(path, "tempNew");
-            save.Start();
-            save.WaitForExit();
-            var Old = GetE3Data(path + "\\temp\\tempOld.csv");
-            var New = GetE3Data(path + "\\temp\\tempNew.csv");
-            var Temp = New.GetRange(Old.Count, New.Count - Old.Count);
-            Dictionary<string, List<E3Data>> InactivityState = new Dictionary<string, List<E3Data>>();
-            foreach (var item in Temp)
-            {
-                if (InactivityState.ContainsKey(item.InteractivityState)) 
-                {
-                    InactivityState[item.InteractivityState].Add(item);
-                }
-                else
-                {
-                    InactivityState.Add(item.InteractivityState, new List<E3Data>());
-                    InactivityState[item.InteractivityState].Add(item);
-
-                }
-            }
-            Dictionary<string, List<E3Data>> Inactive = IsolateApps(InactivityState, " NotUnique");
-            Dictionary<string, List<E3Data>> Forcus = IsolateApps(InactivityState, " Focus");
-            Dictionary<string, List<E3Data>> Visible = IsolateApps(InactivityState, " Visible");
-            Dictionary<string, List<E3Data>> Minimized = IsolateApps(InactivityState, " Minimized");
-
-
-            //foreach (var key in InactivityState.Keys) 
-            //{
-            //    InactivityState[key] = InactivityState[key].GroupBy(x=> x.AppId).Select(x => x.First()).ToList();
-            //}
-
-            List<int> small = New.Select(x => int.Parse(x.TimeInMSec)).ToList();
-            
-            small.Sort();
+            string path = Constants.GetPathForSource(_source.ToString())+"\\temp\\";
+            var Final = GetE3Data(path, "2");
+            var Result = Final.Where(x => GetTimeStamp(x).CompareTo(PrevMeasure) > 0).ToList();
             Console.WriteLine("Done");
-
-            //string file = Constants.GetFileName(dateTime);
-            //Process collect = E3StopProcess(path, file);
-            //collect.Start();
-            //collect.WaitForExit();
-        }
-        public Dictionary<string, List<E3Data>> IsolateApps(Dictionary<string, List<E3Data>> dict, string key) 
-        {
-            Dictionary<string, List<E3Data>> valuePairs = new Dictionary<string, List<E3Data>>();
-            if (dict.ContainsKey(key)) 
-            {
-                foreach (var item in dict[key])
-                {
-                    if (valuePairs.ContainsKey(item.AppId))
-                    {
-                        valuePairs[item.AppId].Add(item);
-                    }
-                    else
-                    {
-                        valuePairs.Add(item.AppId, new List<E3Data>());
-                        valuePairs[item.AppId].Add(item);
-
-                    }
-                }            
-            }
-            return valuePairs;
         }
 
         private List<E3Data> GetE3Data(string path) 
         {
-            //List<E3Data> values = File.ReadAllLines(path)
-            //                   .Skip(1)
-            //                   .Select(v => DailyValues.FromCsv(v))
-            //                   .ToList();
             var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 Encoding = Encoding.UTF8,
@@ -211,49 +175,17 @@ namespace EnergyComparer.Profilers
             return process;
         }
 
-        //private Process E3ClearProccesOld() 
+        //private Process E3ClearProccesOld()
         //{
         //    Process process = new Process();
         //    ProcessStartInfo startInfo = new ProcessStartInfo();
         //    startInfo.FileName = "powershell.exe";
         //    startInfo.UseShellExecute = true;
-        //    startInfo.Arguments = $" /C (cd {SruDefaultPath}) | (sc stop dps)| (rm srudb.dat.bak)| (mv .\\SRUDB.dat srudb.dat.bak)| (sc start dps)";
+        //    startInfo.RedirectStandardError = true;
+        //    startInfo.Arguments = $" /C (cd {SruDefaultPath}); (sc stop dps); (rm .\\SRUDB.dat); (sc start dps)";
         //    startInfo.Verb = "runas";
-        //    process.StartInfo = startInfo;
-        //    return process;
-        //}
-
-        //private Process E3StartProcess()
-        //{
-        //    Process process = new Process();
-        //    ProcessStartInfo startInfo = new ProcessStartInfo();
-        //    startInfo.FileName = "powershell.exe";
-        //    startInfo.UseShellExecute = true;
-        //    startInfo.Arguments = $" /C (cd {SruDefaultPath})";
-        //    startInfo.Verb = "runas";
-        //    process.StartInfo = startInfo;
-        //    return process;
-        //}
-        //private Process E3StopProcess(string path, string fileName)
-        //{
-        //    Process process = new Process();
-        //    ProcessStartInfo startInfo = new ProcessStartInfo();
-        //    startInfo.FileName = "powershell.exe";
-        //    startInfo.UseShellExecute = true;
-        //    startInfo.Arguments = $" /C (cd {path.Replace(" ", "` ")}); (powercfg.exe /srumutil  /output {fileName} /csv);";
-        //    startInfo.Verb = "runas";
-        //    process.StartInfo = startInfo;
-        //    return process;
-        //}
-
-        //private Process AdminPowerProcces(string command) 
-        //{
-        //    Process process = new Process();
-        //    ProcessStartInfo startInfo = new ProcessStartInfo();
-        //    startInfo.FileName = "powershell.exe";
-        //    startInfo.UseShellExecute = true;
-        //    startInfo.Arguments = $" /C {command}";
-        //    startInfo.Verb = "runas";
+        //    startInfo.CreateNoWindow = true;
+        //    startInfo.UseShellExecute = false;
         //    process.StartInfo = startInfo;
         //    return process;
         //}
@@ -264,6 +196,7 @@ namespace EnergyComparer.Profilers
             {
                 Encoding = Encoding.UTF8,
                 Delimiter = ",",
+                TrimOptions = TrimOptions.Trim,
             };
             using (var fs = File.Open(path + "\\" + file, FileMode.Open))
             {
@@ -285,11 +218,16 @@ namespace EnergyComparer.Profilers
         (DtoTimeSeries, DtoRawData) IEnergyProfiler.ParseData(string path, int experimentId, DateTime startTime)
         {
             DtoRawData dtoRawData = new DtoRawData();
-            dtoRawData.Value = JsonSerializer.Serialize(GetE3Data(path));
+            dtoRawData.Value = JsonSerializer.Serialize(result);
             dtoRawData.ExperimentId = experimentId;
             dtoRawData.Time = startTime;
 
-            throw new NotImplementedException("Tell Jeppe to implement this");
+            DtoTimeSeries dtoTimeSeries = new DtoTimeSeries();
+            dtoTimeSeries.Value = "{}";
+            dtoTimeSeries.ExperimentId = experimentId;
+            dtoTimeSeries.Time = startTime;
+
+            return (dtoTimeSeries, dtoRawData);
         }
     }
 }
