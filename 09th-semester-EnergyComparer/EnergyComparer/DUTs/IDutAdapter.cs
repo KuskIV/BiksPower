197f0375b816ee2f85a6021c705b31f4528726ac
Jeppe <jeppe@bandsholm.com>
2022-11-14 12:39:47 +0100

    Revert "IDUTAdpter change"
    This reverts commit 5cda4665626e5a521298845d9f3bb94b0d4c7293.

--- 09th-semester-EnergyComparer/EnergyComparer/DUTs/IDutAdapter.cs
+++ 09th-semester-EnergyComparer/EnergyComparer/DUTs/IDutAdapter.cs
@@ -9,6 +9,7 @@ namespace EnergyComparer.DUTs
         int GetChargeRemaining();
         IEnergyProfiler GetDefaultProfiler();
         List<IEnergyProfiler> GetProfilers();
+        IEnergyProfiler GetProfilers(string name);
         float GetTemperature();
         void DisableNetworking(string interfaceName);
         void EnableNetworking(string interfaceName);
